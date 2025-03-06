42d875cfbd1168c7a43792ae7e18aac058b72d99
mxhuy <mxhuy>
2025-03-06 21:15:36 +0700

    set ckpt_freq default to 0

--- train.py
+++ train.py
@@ -63,7 +63,7 @@ def parse_args():
                         help='device to use for training')
     parser.add_argument('--output_dir', default='outputs',
                         help='folder to output images and model checkpoints')
-    parser.add_argument('--ckpt_freq', type=int, default=100,
+    parser.add_argument('--ckpt_freq', type=int, default=0,
                         help='save frequency (epochs)')
     parser.add_argument('--eval_freq', type=int, default=5,
                         help='save frequency (epochs)')
@@ -223,7 +223,7 @@ def main():
         with open(os.path.join(output_dir, 'log.txt'), 'a') as log_f:
             log_f.write(json.dumps(log_stats) + '\n')
         # checkpointing
-        if epoch % params.ckpt_freq == 0 or epoch == params.epochs:
+        if (params.ckpt_freq and epoch % params.ckpt_freq == 0) or epoch == params.epochs:
             torch.save(model.state_dict(), os.path.join(output_dir, f'{params.model}_{epoch:03d}.pth'))
         # visualize
         if params.visualize and epoch % params.vis_freq == 0:
